7fa4669631ab312eb972fad9a0f0076dd55473ff
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-10-31 14:08:16 -0700

    heroku

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -32,9 +32,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'A Web Developer needs a text editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
